16bf4cb0af4f4ff232dfea5a2c733ba4d6c43050
Ameen Popal <ameen@popal.net>
2026-07-30 16:56:10 -0400

    Allow Vercel frontend with CORS

--- backend/main.py
+++ backend/main.py
@@ -61,7 +61,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://get-synced-two.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
